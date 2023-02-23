343ba148b60234c5157709e5ef2ae7dbe5e03ba4
ananya150 <akblockchain15@gmail.com>
2023-02-24 02:46:31 +0530

    added Mongo class

--- src/Database/mongo.py
+++ src/Database/mongo.py
@@ -1,5 +1,63 @@
-import numpy as np
-import numba as jit
-from multiprocessing.pool import ThreadPool
-
+import logging
 from pymongo import MongoClient
+
+logging.basicConfig(filename="newfile.log",
+                    format='%(asctime)s %(message)s',
+                    filemode='w')
+
+logger = logging.getLogger()
+
+
+class Mongo():
+
+    """
+        Connect to MongoDb
+    """
+    def __init__(self , mgClient) -> None:
+
+        try:
+            self.client = MongoClient(mgClient)
+            logger.info("Connected to mongodb")
+            self.db = self.client["obdb"]
+            self.coll = self.db["orderbook"]
+            self.connection = True
+
+        except:
+            logger.debug("Error in connecting to mongodb")
+            self.connection = False
+
+
+    """
+        Inserts the data to mongodb in specified format
+    """
+    def appendMongo(self , obj): 
+
+        dict = {
+            "timestamp": (obj.timestamp),
+
+            "top_bid": str(obj.top_bid),
+            "top_ask": str(obj.top_ask),
+            "mid_point": str(obj.mid_point),
+
+            "tot_bidSize": str(obj.tot_bidSize),
+            "tot_askSize": str(obj.tot_askSize),
+
+            "imbalance": str(obj.imbalance),
+            "orderbook_pressure": str(obj.pressure),
+            "weighted_midpoint": str(obj.weighted_midpoint)
+        }
+        resp = self.coll.insert_one(dict)
+        logger.info("Response added" , resp)
+        return 0
+
+
+    """
+        Fetches data from mongodb
+    """
+    def fetchMongo(self , stamp):
+
+        resp =  self.coll.find({"timestamp": {"$gt": stamp} })
+        return resp
+
+    
+
